8a2476c1e5a40ab01909443c9e8aa11615a3eaeb
YaJunCui <644583255@qq.com>
2018-08-17 18:34:57 +0800

    修改

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,3 +3,9 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 
 from . import views, errors   # 避免循环导入依赖，因为在views.py和errors.py中要导入蓝本main
+from ..models import Permission
+
+# 将Permission类加入模板上下文
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
