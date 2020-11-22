b0b08a6685e429994948742d7a3a70ecf5807e0d
YaJunCui <644583255@qq.com>
2020-11-22 22:24:51 +0800

    修改

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__)  # 创建名为main的蓝本
 
 from . import views, errors   # 避免循环导入依赖，因为在views.py和errors.py中要导入蓝本main
 from ..models import Permission
